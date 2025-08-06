d0c7ae70ea3f9c40a2f061be14e0958266b02a1a
Darshan-gowda-m <155784473+Darshan-gowda-m@users.noreply.github.com>
2025-08-07 00:59:54 +0530

    Create 10a.java

---
+++ 10a.java
@@ -0,0 +1,37 @@
+package com.greet;
+
+
+import java.io.*;
+import javax.servlet.*; 
+import javax.servlet.annotation.WebServlet; 
+import javax.servlet.http.*;
+
+@WebServlet("/GreetServlet")
+public class GreetServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("<html><head><title>Greeting</title></head><body>");
+        out.println("<h1>Hello, " + name + "!</h1>");
+        out.println("</body></html>");
+    }
+}
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head><title>Greeting App</title></head>
+<body>
+    <form action="GreetServlet" method="post">
+        <h2>Enter Your Name</h2>
+        <input type="text" name="name" required><br><br>
+        <input type="submit" value="Greet Me">
+    </form>
+</body>
+</html>
